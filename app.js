dbd0f3cf15c584f31b5417fd41faa4b7043af99d
LanTran2006 <108577658+LanTran2006@users.noreply.github.com>
2022-10-14 12:11:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,45 +2,45 @@ const musics = [
     {
       id: 1,
       title: "Holo",
-      file: "holo.mp3",
+      file: "/song/holo.mp3",
       image:
         "https://images.unsplash.com/photo-1614624532983-4ce03382d63d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80",
     },
     {
       id: 2,
       title: "Summer",
-      file: "mp3_music_summer.mp3",
+      file: "/song/mp3_music_summer.mp3",
       image:
         "https://images.unsplash.com/photo-1616763355548-1b606f439f86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
     },
     {
         id: 3,
       title: "spark",
-      file: "spark.mp3",
+      file: "/song/spark.mp3",
       image:"https://images.unsplash.com/photo-1665326330373-e1ca5ef6f2ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
     },
     {
         id:4,
         title: "home",
-        file: "home.mp3",
+        file: "/song/home.mp3",
         image: "https://images.unsplash.com/photo-1665366405948-0a49fdbde80f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
     },
    {
     id:5,
     title: "Our Story",
-    file: "ChuyenDoiTa-EmceeLDaLAB-7120974.mp3",
+    file: "/song/ChuyenDoiTa-EmceeLDaLAB-7120974.mp3",
     image: "https://avatar-ex-swe.nixcdn.com/song/share/2021/11/25/c/0/d/d/1637809827065.jpg"
    },
    {
     id:6,
     title: "Hey",
-    file: "music-player_music_hey.mp3",
+    file: "/song/music-player_music_hey.mp3",
     image: "https://images.unsplash.com/photo-1665472832769-c8bffec9296f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
    },
    {
     id:7,
     title: "Ukulele",
-    file: "music-player_music_ukulele.mp3",
+    file: "/song/music-player_music_ukulele.mp3",
     image: "https://images.unsplash.com/photo-1665412019489-1928d5afa5cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
    }
   ];
@@ -86,8 +86,10 @@ function set_time() {
         let {currentTime,duration}=song;
         let rangebar_value=(currentTime/duration)*100;
         progress.value=rangebar_value;
+       if (currentTime && duration) {
         current_time.innerHTML=format_time(currentTime)
         duration_time.innerHTML=format_time(duration)
+       }
         if (currentTime==duration) {
             if (!isrepeat) {
                change_song(1)
@@ -134,10 +136,10 @@ function select_song() {
      index_song=parseInt(this.className.slice(-1))
      thumb.style=`background-image: url("${musics[index_song].image}")`
      song.src=musics[index_song].file
+     reset_time()
      if (isplaying)  song.play()
      this.classList.add('active')
      progress.value=0;
-     reset_time()
 }
 thumb.style=`background-image: url("${musics[index_song].image}")`
 play_btn.addEventListener('click',()=> {
